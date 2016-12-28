b8152686b216d269510d32145ce64edf0006533b
21g <lu.liu@msxf.com>
2016-12-28 14:24:33 +0800

    create

---
+++ 乐游/leyou/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  leyou
+//
+//  Created by lu.liu on 2016/12/28.
+//  Copyright © 2016年 lu.liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
